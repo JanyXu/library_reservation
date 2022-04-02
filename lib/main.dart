d030255e71966137857f1334a1507b6f168518ab
JanyXu <568699213@qq.com>
2022-04-02 14:31:47 +0800

    zzh main commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo2 test1',
+      title: '扫码助手',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
