235caf279352ea5a526788504428c416980e7e31
周子涵 <634125407@qq.com>
2022-04-02 13:56:39 +0800

    key setting

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo2',
+      title: 'Flutter Demo2 test',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
