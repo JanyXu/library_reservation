a6e960ef0922a0214ef218f0e5f90c007300af79
AKunMan <634125407@qq.com>
2022-04-09 09:48:09 +0800

    删除dialog背景图片

---
+++ lib/ui/widgets/serial_number_dialog/serial_dialog.dart
@@ -0,0 +1,1 @@
+import 'package:flutter/material.dart';class SCSerialDialogWidget extends StatelessWidget {  const SCSerialDialogWidget({Key? key}) : super(key: key);  @override  Widget build(BuildContext context) {    return Container();  }}
